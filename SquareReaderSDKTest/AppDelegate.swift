5f003ff65bfa3c64da0c6754d6088bf1534d5596
jjc235 <jjc@kios.cloud>
2020-01-15 19:41:34 -0800

    Upload complete project

--- SquareReaderSDKTest/AppDelegate.swift
+++ SquareReaderSDKTest/AppDelegate.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import SquareReaderSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        SQRDReaderSDK.initialize(applicationLaunchOptions: launchOptions)
+
         return true
     }
 
